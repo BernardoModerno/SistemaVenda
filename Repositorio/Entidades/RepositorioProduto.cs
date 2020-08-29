f84b136eb74292a6d6a1eba129f0271d0c0de1e9
BernardoModerno <bernardo.moderno@gmail.com>
2020-08-29 17:50:51 -0300

    Arquitetura em Camadas (Parte 5) Produtos

---
+++ Repositorio/Entidades/RepositorioProduto.cs
@@ -0,0 +1,16 @@
+﻿using Dominio.Repositorio;
+using Microsoft.EntityFrameworkCore;
+using Repositorio.Contexto;
+using Repositorio.Interfaces;
+using SistemaVenda.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorio.Entidades
+{
+    public class RepositorioProduto : Repositorio<Produto>, IRepositorioProduto
+    {
+        public RepositorioProduto(ApplicationDbContext dbContext) : base(dbContext) { }
+    }
+}
--- SistemaVenda/Startup.cs
+++ SistemaVenda/Startup.cs
@@ -52,14 +52,17 @@ namespace SistemaVenda
             //Serviço Aplicação
             services.AddScoped<IServicoAplicacaoCategoria, ServicoAplicacaoCategoria>();
             services.AddScoped<IServicoAplicacaoCliente, ServicoAplicacaoCliente>();
+            services.AddScoped<IServicoAplicacaoProduto, ServicoAplicacaoProduto>();
 
             //Dominio
             services.AddScoped<IServicoCategoria, ServicoCategoria>();
             services.AddScoped<IServicoCliente, ServicoCliente>();
+            services.AddScoped<IServicoProduto, ServicoProduto>();
 
             //Repositorio
             services.AddScoped<IRepositorioCategoria, RepositorioCategoria>();
             services.AddScoped<IRepositorioCliente, RepositorioCliente>();
+            services.AddScoped<IRepositorioProduto, RepositorioProduto>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
